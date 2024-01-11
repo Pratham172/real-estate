81b73d580bc31d17505d7219b6177e52c4ef2d2b
Pratham172 <prathamkulal19@gmail.com>
2024-01-11 12:09:31 +0530

    connected to mongo database

---
+++ Readme.md
@@ -0,0 +1,8 @@
+functional documentation backend
+
+1. index.js
+
+* import express
+* Run server on port 7000
+* import mongoose
+* connect to Mongo Database using mongoose.connect
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.listen(7000, ()=> {
     console.log("server is running on port 7000!!");
+});
+
+const mongoUrl = "mongodb://127.0.0.1/real_estate"
+mongoose.connect(mongoUrl)
+.then(()=> {
+    console.log("-----------------")
+    console.log("Connected to MongoDB")
+})
+.catch((err) => {
+    console.log(err);
 });
