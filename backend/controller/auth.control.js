401d7a54e6e1c10058bc6837a87dd105c290251e
Pratham172 <prathamkulal19@gmail.com>
2024-01-12 19:13:49 +0530

    created sign in backend api route

--- backend/controller/auth.control.js
+++ backend/controller/auth.control.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup = async (req, res, next) => {
 
@@ -13,4 +16,26 @@ export const signup = async (req, res, next) => {
     catch (error) {
         next(error);
     }
-}
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "User Not Found"));
+        }
+        const validPass = bcryptjs.compareSync(password, validUser.password);
+        if (!validPass) {
+            return next(errorHandler(401, "Please try to login with correct credentials"));
+        }
+        const token = jwt.sign({ id: validUser._id, }, process.env.JWT_SECRET);
+        const { password: pass, ...rest} = validUser._doc;
+        res.cookie('token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controller/auth.control.js';
+import { signin, signup } from '../controller/auth.control.js';
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
+router.post("/sign-in", signin);
 
 export default router;
