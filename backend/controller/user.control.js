3728c7ed9f27674c0451165e6ddae734b49d4957
Pratham172 <prathamkulal19@gmail.com>
2024-01-21 22:52:14 +0530

    completed show user api route

--- backend/controller/user.control.js
+++ backend/controller/user.control.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/custom.error.js";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) return next(errorHandler(401, "you are not authunticated!"));
@@ -38,5 +39,17 @@ export const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
 
-}
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+  };
